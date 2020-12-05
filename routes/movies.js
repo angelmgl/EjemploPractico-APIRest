5da0cc1110d5cc8a61111f3e80f84712216711d3
Angel Mgl <angelemegeele@gmail.com>
2020-12-05 14:22:14 -0300

    Servidor configurado con Babel y Consign, operaciones CRUD completadas

---
+++ routes/movies.js
@@ -0,0 +1,50 @@
+import mongojs from 'mongojs';
+
+const db = mongojs('DatabaseMovies', [
+    'movies'
+]);
+
+module.exports = app => {
+
+    app.get('/movies', (req, res) => {
+        db.movies.find((err, movies) => {
+            res.json({
+                movies
+            });
+        });
+    });
+
+    app.post('/movies', (req, res) => {
+        let newMovie = req.body;
+
+        db.movies.insert(newMovie, (err, movie) => {
+            res.json({
+                movie
+            });
+        });
+    });
+
+    app.put('/movies/:id', (req, res) => {
+        let updatedMovie = req.body;
+
+        db.movies.updateOne(
+            { _id: mongojs.ObjectId(req.params.id) }, 
+            { $set: updatedMovie }, 
+            {}, 
+            (err, movie) => {
+                res.json({
+                    movie
+            });
+        });
+    });
+
+    app.delete('/movies/:id', (req, res) => {
+        db.movies.remove({
+            _id: mongojs.ObjectId(req.params.id)
+        }, (err, response) => {
+            res.json({
+                response
+            });
+        });
+    });
+};
